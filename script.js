84d73696d7941cf7c53f84a2cf2ce782cb537fbc
Tzarius1989 <73390651+Tzarius1989@users.noreply.github.com>
2024-08-08 21:17:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const tg = window.Telegram.WebApp;
 
 document.addEventListener('DOMContentLoaded', () => {
     tg.ready();
-    
     const buyButton = document.getElementById('buyButton');
     buyButton.addEventListener('click', handleBuyClick);
 });
@@ -12,19 +11,16 @@ function handleBuyClick() {
         title: 'Подтвердите покупку',
         message: 'Вы хотите купить "Feline Fedora" в Cat Shelter за 55 Stars?',
         buttons: [
-            { id: 'buy', type: 'ok', text: 'Подтвердить и оплатить ⭐ 55' },
+            { id: 'buy', type: 'buy', text: 'Подтвердить и оплатить ⭐ 55' },
             { type: 'cancel' }
         ]
     }, (buttonId) => {
         if (buttonId === 'buy') {
-            const purchaseData = {
+            tg.sendData(JSON.stringify({
                 action: 'buy',
                 product: 'Feline Fedora',
                 amount: 55
-            };
-
-            tg.sendData(JSON.stringify(purchaseData));
-            tg.close();
+            }));
         }
     });
 }
